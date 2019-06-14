598da45f702551383bed8e680088bd0db74342a4
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-06-15 02:32:28 +0300

    Fix like migration to return the total_likes
    This commit fixes the like migration to enable the
    user to see the total number of likes specific to a
    repository and not all the likes within the likes
    table.

--- knex/migrations/20190612203816_alter_likes_constrant.js
+++ knex/migrations/20190612203816_alter_likes_constrant.js
@@ -11,21 +11,23 @@ const ON_INSERT_LIKE_FUNCTION = `
             DELETE FROM likes WHERE user_id = uid AND repo_id = rid;
             SELECT COUNT(*) AS total_likes, 
             EXISTS(
-                SELECT * 
-                FROM likes 
+                SELECT *
+                FROM likes
                 WHERE user_id = uid
                 AND repo_id = rid
-            ) AS liked INTO likes_obj FROM likes;
+            ) AS liked INTO likes_obj
+            FROM likes 
+            WHERE repo_id = rid;
             RETURN likes_obj;
         ELSE
             INSERT INTO likes(user_id, repo_id) VALUES(uid, rid);
             SELECT COUNT(*) AS total_likes, 
             EXISTS(
-                SELECT * 
-                FROM likes 
+                SELECT *
+                FROM likes
                 WHERE user_id = uid
                 AND repo_id = rid
-            ) AS liked INTO likes_obj FROM likes;
+            ) AS liked INTO likes_obj FROM likes WHERE repo_id = rid;
             RETURN likes_obj;
         END IF;
     END;
