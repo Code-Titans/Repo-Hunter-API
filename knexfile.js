8cd0b4af745d4f850d801000870a6f306bc6e420
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-08-01 23:12:09 +0300

    add host to knexfile for deployment

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,23 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: process.env.PGHOST || '127.0.0.1',
       user: process.env.PGUSER || 'postgres',
       password: process.env.PGPASSWORD || '',
       database: process.env.PGDATABASE || 'repohunter',
+      charset: 'utf8',
+    },
+    migrations: {
+      directory: `${__dirname}/knex/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/knex/seeds`,
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connection: process.env.DATABASE_URL,
       charset: 'utf8',
     },
     migrations: {
